0294d7769a6ab0175f2d8700f30ad451e6236707
Liii <133943399@qq.com>
2022-11-15 12:00:21 +0800

    [update]

--- app/Http/Controllers/PushController.php
+++ app/Http/Controllers/PushController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Events\MessageEvent;
 use Illuminate\Http\Request;
-use mysql_xdevapi\Exception;
 
 class PushController extends Controller
 {
@@ -13,34 +12,9 @@ class PushController extends Controller
     {
         $msg = $request->msg;
         $user_id = $request->user_id;
-
-//        event(new MessageEvent($user_id, $msg));
-//        return response()->json([
-//            'status' => $msg,
-//        ]);
-        try {
-
-
-            $pusher = new \Pusher\Pusher(
-                env('PUSHER_APP_KEY'),
-                env('PUSHER_APP_SECRET'),
-                env('PUSHER_APP_ID'),
-                ['cluster' => env('PUSHER_APP_CLUSTER'),]
-            );
-
-
-            $a = $pusher->trigger('message', 'send_to_user_' . $user_id, [
-                'date'    => date('Y-m-d H:i:s', time()),
-                'text'    => $msg,
-                'mine'    => false,
-                'name'    => \Auth::user()->name,
-                'user_id' => \Auth::user()->toArray(),
-            ]);
-        }catch (\Exception $e){
-            $a = $e->getMessage();
-        }
+        event(new MessageEvent($user_id,$msg));
         return response()->json([
-            'status' => $a,
+            'status' => $msg,
         ]);
     }
 
