cd9da0005f0f35340ce152cb895ce97b219dcdbf
Liii <133943399@qq.com>
2022-11-15 11:55:12 +0800

    [update]

--- app/Http/Controllers/PushController.php
+++ app/Http/Controllers/PushController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Events\MessageEvent;
 use Illuminate\Http\Request;
+use mysql_xdevapi\Exception;
 
 class PushController extends Controller
 {
@@ -17,23 +18,27 @@ class PushController extends Controller
 //        return response()->json([
 //            'status' => $msg,
 //        ]);
-
-        $pusher = new \Pusher\Pusher(
-            env('PUSHER_APP_KEY'),
-            env('PUSHER_APP_SECRET'),
-            env('PUSHER_APP_ID'),
-            ['cluster' => env('PUSHER_APP_CLUSTER'),]
-        );
+        try {
 
 
-        $a = $pusher->trigger('message', 'send_to_user_'.$user_id, [
-            'date' => date('Y-m-d H:i:s', time()),
-            'text' => $msg,
-            'mine' => false,
-            'name' => \Auth::user()->name,
-            'user_id' => \Auth::user()->toArray(),
-        ]);
+            $pusher = new \Pusher\Pusher(
+                env('PUSHER_APP_KEY'),
+                env('PUSHER_APP_SECRET'),
+                env('PUSHER_APP_ID'),
+                ['cluster' => env('PUSHER_APP_CLUSTER'),]
+            );
 
+
+            $a = $pusher->trigger('message', 'send_to_user_' . $user_id, [
+                'date'    => date('Y-m-d H:i:s', time()),
+                'text'    => $msg,
+                'mine'    => false,
+                'name'    => \Auth::user()->name,
+                'user_id' => \Auth::user()->toArray(),
+            ]);
+        }catch (\Exception $e){
+            \Log::error($e->getMessage());
+        }
         return response()->json([
             'status' => $a,
         ]);
